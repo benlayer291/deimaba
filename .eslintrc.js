9c448b4df477018825c159a609efe895022b4279
Ben Layer <benlayer291@gmail.com>
2018-09-23 11:36:38 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,18 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'airbnb-base',
+    'plugin:vue/recommended',
   ],
   // required to lint *.vue files
   plugins: [
+    'import',
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'global-require': 'off',
+    'import/no-unresolved': 'off',
+    'semi': [2, 'never'],
+  }
 }
